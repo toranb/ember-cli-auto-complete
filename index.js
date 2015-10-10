8d62e57f16a4095b2b7bcd295063ead3e6839c01
Anthony Gore <anthony@alliedpublishers.com.au>
2015-10-11 09:27:07 +1100

    Updating included function so this addon can be used within another addon

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var path = require('path');
 module.exports = {
     name: 'ember-cli-auto-complete',
     included: function(app) {
-        app.import(path.join('vendor', 'auto-complete.css'));
+    	if (app.app) {
+	        app = app.app;
+	    }
+	    this.app = app;
+	    app.import(path.join('vendor', 'auto-complete.css'));
     }
 };
